38d9e724543caf9574f0e5aab0049c94026c63c3
Liubomyr Tsimbolynets <l.tsimbolynets-c@tamkeentech.sa>
2023-05-11 14:27:08 +0300

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CheckoutComponent } from 'src/app/checkout/checkout.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'checkout', component: CheckoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
